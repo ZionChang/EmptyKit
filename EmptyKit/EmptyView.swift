1f65a70c38800061111394e5c1be3bfa73079a7a
archerzhz <zhangzhi@yeteam.com>
2016-12-22 15:50:10 +0800

    Fix emptyView subviews properties when resue

--- EmptyKit/EmptyView.swift
+++ EmptyKit/EmptyView.swift
@@ -28,73 +28,20 @@ final class EmptyView: UIView {
     var minimumButtonWidth: CGFloat? = nil
     var fadeInOnDisplay: Bool = true
     
+    let contentView: UIView = UIView()
+    var titleLabel: UILabel!
+    var detailLabel: UILabel!
+    var imageView: UIImageView!
+    var button: UIButton!
+    
     // Events
     var didTappedEmptyView:((UIView) -> Void)?
     var didTappedButton:((UIButton) -> Void)?
     
-    // MARK: - Lazy Properties
-    lazy var contentView: UIView = { [unowned self] in
-        let newView = UIView()
-        newView.translatesAutoresizingMaskIntoConstraints = false
-        newView.backgroundColor = UIColor.clear
-        newView.isUserInteractionEnabled = true
-        newView.alpha = 0
-        return newView
-    }()
-    
-    lazy var titleLabel: UILabel = { [unowned self] in
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.autoresizingMask = [.flexibleWidth]
-        label.backgroundColor = .clear
-        label.font = UIFont.systemFont(ofSize: 27)
-        label.textColor = UIColor(white: 0.6, alpha: 1)
-        label.textAlignment = .center
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        label.accessibilityLabel = "empty set title"
-        return label
-    }()
-    
-    lazy var detailLabel: UILabel = { [unowned self] in
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .clear
-        label.font = UIFont.systemFont(ofSize: 17)
-        label.textColor = UIColor(white: 0.6, alpha: 1)
-        label.textAlignment = .center
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        label.accessibilityLabel = "empty set detail label"
-        return label
-    }()
-    
-    lazy var imageView: UIImageView = { [unowned self] in
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.backgroundColor = UIColor.clear
-        imageView.contentMode = .scaleAspectFit
-        imageView.isUserInteractionEnabled = false
-        imageView.accessibilityIdentifier = "empty set background image"
-        return imageView
-    }()
-    
-    lazy var button: UIButton = { [unowned self] in
-        let button = UIButton(type: .custom)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.contentHorizontalAlignment = .center
-        button.contentVerticalAlignment = .center
-        button.accessibilityIdentifier = "empty set button"
-        button.addTarget(self, action: #selector(EmptyView.didTapButton), for: .touchUpInside)
-        return button
-    }()
-    
     // MARK: - Initialization
     init() {
         super.init(frame: CGRect.zero)
-        addSubview(contentView)
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapEmptyView(_:)))
-        addGestureRecognizer(tapGesture)
+        setupContentView()
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -135,6 +82,7 @@ internal extension EmptyView {
             $0.removeFromSuperview()
         }
         self.removeAllConstraints()
+        setupContentSubviews()
     }
     
 }
@@ -149,9 +97,73 @@ private extension EmptyView {
     
 }
 
-
 // MARK: - Setup
 internal extension EmptyView {
+    
+    func setupContentSubviews() {
+        setupTitleLable()
+        setupDetailLabel()
+        setupButton()
+        setupImageView()
+    }
+    
+    func setupContentView() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = UIColor.clear
+        contentView.isUserInteractionEnabled = true
+        contentView.alpha = 0
+        addSubview(contentView)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapEmptyView(_:)))
+        addGestureRecognizer(tapGesture)
+        setupContentSubviews()
+    }
+    
+    func setupTitleLable() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.autoresizingMask = [.flexibleWidth]
+        label.backgroundColor = .clear
+        label.font = UIFont.systemFont(ofSize: 27)
+        label.textColor = UIColor(white: 0.6, alpha: 1)
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.accessibilityLabel = "empty set title"
+        self.titleLabel = label
+    }
+    
+    func setupDetailLabel() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textColor = UIColor(white: 0.6, alpha: 1)
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.accessibilityLabel = "empty set detail label"
+        self.detailLabel = label
+    }
+    
+    func setupImageView() {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.clear
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = false
+        imageView.accessibilityIdentifier = "empty set background image"
+        self.imageView = imageView
+    }
+    
+    func setupButton() {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.contentHorizontalAlignment = .center
+        button.contentVerticalAlignment = .center
+        button.accessibilityIdentifier = "empty set button"
+        button.addTarget(self, action: #selector(EmptyView.didTapButton), for: .touchUpInside)
+        self.button = button
+    }
     
     func setupConstraints() {
         let space = self.verticalSpace
